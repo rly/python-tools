00e0ea7fffb0fdaf2ea343e2d34ae7ec7c588f9f
Ryan Ly <rly@lbl.gov>
2019-04-11 11:47:32 -0700

    fix find and rename behavior -- it was selecting only definition and current usage

--- lib/jedi/evaluate/usages.py
+++ lib/jedi/evaluate/usages.py
@@ -37,7 +37,7 @@ def _find_names(module_context, tree_name):
 
 def usages(module_context, tree_name):
     search_name = tree_name.value
-    found_names = _find_names(module_context, tree_name)
+    found_names = _find_names(module_context, tree_name) # dict of the goto usage and the current selection
     modules = set(d.get_root_context() for d in found_names.values())
     modules = set(m for m in modules if isinstance(m, ModuleContext))
 
@@ -62,4 +62,26 @@ def usages(module_context, tree_name):
     return found_names.values()
 
 def usages_in_module(module_context, tree_name):
-    return _find_names(module_context, tree_name).values()
+    search_name = tree_name.value
+    found_names = _find_names(module_context, tree_name) # dict of the goto usage and the current selection
+    modules = set(d.get_root_context() for d in found_names.values())
+    modules = set(m for m in modules if isinstance(m, ModuleContext))
+
+    non_matching_usage_maps = {}
+    for name_leaf in module_context.tree_node.get_used_names().get(search_name, []):
+        new = _find_names(module_context, name_leaf)
+        if any(tree_name in found_names for tree_name in new):
+            found_names.update(new)
+            for tree_name in new:
+                for dct in non_matching_usage_maps.get(tree_name, []):
+                    # A usage that was previously searched for matches with
+                    # a now found name. Merge.
+                    found_names.update(dct)
+                try:
+                    del non_matching_usage_maps[tree_name]
+                except KeyError:
+                    pass
+        else:
+            for name in new:
+                non_matching_usage_maps.setdefault(name, []).append(new)
+    return found_names.values()
